c12d8f051d90bd9bb38996126bf344db081b400d
Derek Pan <ustcpxy@gmail.com>
2018-03-22 18:04:17 +0800

    support code completion

--- config.el
+++ config.el
@@ -31,3 +31,26 @@
       )
     )
   (advice-add #'xref-pop-marker-stack :around #'+lookup*xref-pop-marker-stack))
+
+;; *** Company
+(after! company
+  (setq company-tooltip-limit 10
+        company-minimum-prefix-length 2
+        company-idle-delay 0.2
+        company-tooltip-minimum-width 60
+        company-tooltip-margin 0
+        company-show-numbers t
+        company-tooltip-offset-display nil
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case nil
+        company-dabbrev-code-other-buffers t
+        company-tooltip-align-annotations t
+        company-require-match 'never
+        company-global-modes '(not comint-mode erc-mode message-mode help-mode gud-mode)
+        company-childframe-child-frame nil))
+(set! :company-backend '(emacs-lisp-mode) '(company-elisp company-files company-yasnippet company-dabbrev-code))
+(set! :company-backend '(python-mode) '(company-anaconda company-files company-yasnippet company-dabbrev-code))
+(set! :company-backend '(inferior-python-mode) '(company-capf company-files company-yasnippet company-dabbrev-code))
+(set! :company-backend '(inferior-ess-mode) '(company-capf company-files company-yasnippet company-dabbrev-code))
+(set! :company-backend '(org-mode) '(company-capf company-files company-yasnippet company-dabbrev))
+(set! :lookup 'emacs-lisp-mode :documentation #'helpful-at-point)
