f9ece36180ce87fa014668ff46294f5d50060d03
Derek Pan <ustcpxy@gmail.com>
2018-03-23 17:09:59 +0800

    remove useless modules

--- +bindings.el
+++ +bindings.el
@@ -6,9 +6,20 @@
 
  :v "M-;" #'comment-dwim
  :gnime "M-;" #'comment-line
+ :gnime "M-'" #'comment-dwim
 
  ;; --- Personal vim-esque bindings ------------------
  :nm "gd" #'+lookup/definition
  :nm "gr" #'+lookup/references
+
+ :ne "M-`"   #'swiper
+ :nv "M-i"   #'imenu
+
+ (:leader
+   ;; Most commonly used
+   :desc "Find file in system"     :n "l" #'counsel-locate
+   :desc "Switch buffer"           :n ","   #'switch-to-buffer
+   :desc "Jump char"               :n "SPC"   #'avy-goto-word-or-subword-1
+   )
  )
 
