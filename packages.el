005f397f69cc0366506b1a5685045c9927e120b5
Derek Pan <ustcpxy@gmail.com>
2019-04-24 19:31:17 +0800

    don't disable package cmake-mode

--- packages.el
+++ packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- description -*- lexical-binding: t; -*-
 
-(disable-packages! cmake-mode company-irony company-irony-c-headers flycheck-irony irony irony-eldoc ivy-rtags rtags)
+(disable-packages! company-irony company-irony-c-headers flycheck-irony irony irony-eldoc ivy-rtags rtags)
 (package! gxref)
 (package! winum)
 (package! ox-hugo)
